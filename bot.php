56d3387c622c5816f4464e3b5eb69bdb4f269721
Ryan Kaufman <130701478+Reggaenomics@users.noreply.github.com>
2023-04-16 01:45:36 -0500

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,14 @@
+<?php
+include __DIR__.'/vendor/autoload.php';
+$discord = new \Discord\Discord([
+    'token' => 'MTA5NzAyNjM3MzI3MDM4ODgyNg.Ga6i0X.e6KtcWWEsuxWsqOY4ZET4urwwpAJTs1VdaxZLY',
+]); //Initializing the bot here
+$discord->on('ready', function ($discord) {
+    echo "Ready to read, readers?", PHP_EOL;
+ //Successful initialization and now awaiting input
+ $discord->on('message', function ($message) {
+       echo "Receiving correspondence from {$message->author->username}: {$message->content}", PHP_EOL;
+   }); //Input received, displaying for to show it has properly read everything.
+});
+$discord->run();
+?>
