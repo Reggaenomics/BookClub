a1c4f96ca14e9b72d64b60fe5160284ec35c7689
Reggae <reggaenomics@COCK.localdomain>
2023-05-18 00:09:32 -0500

    Modified Books, removed BookInput

--- BookInput.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Please enter a book to vote on: \n"; //Prompt user for book title
-$title = readline();
-
-echo "Your book:\n" . $title . "\n"; //make sure I did this right
-return $title; //Should return for the main file(s) to use
-?>
--- Books.php
+++ Books.php
@@ -1,37 +1,63 @@
 <?php
-//////////////////////////////////// //Beginning setup/API stuff here
-$apiKey = 'AIzaSyCYSt4fgIPlrfsa38Z1iBYe6d5DwzeNNl8';
-////////////////////////////////////
+////////////////SETUP/API STUFF START////////////////////
+require_once 'config.php'; //Getting the API Key from Config
+/////////////////SETUP/API STUFF END/////////////////////
+//This was bigger before.
 
-$numberOfPeople = (int) $_POST['number_of_people']; //We need to loop for x amount of people.
+$numberOfPeople = 0;
+
+while ($numberOfPeople <= 0) 
+{
+    echo "How many people are submitting books?: \n"; //Prompt the user to enter x number of people in the book club
+    $numberOfPeople = (int) fgets(STDIN); //We need to loop for x amount of people.
+    if ($numberOfPeople > 0) 
+    {
+        // Valid input, proceed with the rest of the code
+        break;
+    } 
+    else 
+    {
+        // Invalid input, display an error message
+        echo "Invalid input. Please enter a positive number." . PHP_EOL;
+    }
+}
+
 $books = []; //Array to store the books
 
-$url = "https://www.googleapis.com/books/v1/volumes?q=" . urlencode($title);
-$response = file_get_contents($url);
-
-//Parse the XML response
-$data = json_decode($response, true);
-
+// Prompt the user to enter the book titles
 for ($i = 0; $i < $numberOfPeople; $i++) 
 {
-//Time to see if we pulled a novel with the title given
-if ($data['totalItems'] > 0) {
-    // Get the first book in the search results
-    $book = $data['items'][0]['volumeInfo'];
-
-    // Extract the desired information
-    $author = $book['authors'][0];
-    $publicationDate = $book['publishedDate'];
-    $coverUrl = $book['imageLinks']['thumbnail'];
-
-    // Output the book information
-    echo "Title: " . $title . "\n";
-    echo "Author: " . $author . "\n";
-    echo "Publication Date: " . $publicationDate . "\n";
-    echo "Cover: " . $coverUrl . "\n";
-} else {
-    // No books found
-    echo "No books found for the given title.";
+    echo "Enter a book title: ";
+    $books[] = trim(fgets(STDIN));
 }
-} //I NEED TO FIX THIS TO CONSOLIDATE ALL OF THE TITLE LOOPING INTO BOOKINPUT.PHP
+
+foreach ($books as $title) //Simple Loop to read out all the data pulled from Books
+{
+    $url = "https://www.googleapis.com/books/v1/volumes?q=" . urlencode($title);
+    $response = file_get_contents($url);
+
+    //Parse the JSON response
+    $data = json_decode($response, true);
+
+    //Time to see if we pulled a novel with the title given
+    if ($data['totalItems'] > 0) {
+        // Get the first book in the search results
+        $book = $data['items'][0]['volumeInfo'];
+
+        // Extract the desired information
+        $author = $book['authors'][0];
+        $publicationDate = $book['publishedDate'];
+        $coverUrl = $book['imageLinks']['thumbnail'];
+
+        // Output the book information
+        echo "Title: " . $title . "\n";
+        echo "Author: " . $author . "\n";
+        echo "Publication Date: " . $publicationDate . "\n";
+        //echo "Cover: " . $coverUrl . "\n"; //Commenting this out as I currently only see text output
+        echo "////////////////////////\n";
+    } else {
+        // No books found
+        echo "No books found for the given title: " . $title . "\n";
+    }
+}
 ?>
